fa0862a81281c522b4f4290b5fef74c8539723f7
JGuardadoSV <47751227+JGuardadoSV@users.noreply.github.com>
2021-06-10 14:25:15 -0600

    insert completo

--- CRUDMONGO/Program.cs
+++ CRUDMONGO/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace CRUDMONGO
@@ -17,11 +18,19 @@ namespace CRUDMONGO
 
             var ListadoBDS = cliente.ListDatabases().ToList();
             Console.WriteLine("Listado de bases de datos existentes");
-           
+
             foreach (var b in ListadoBDS)
             {
-                Console.WriteLine(b);
+                // Console.WriteLine(b);
             }
+
+            var basededatos = cliente.GetDatabase("agenda");
+            var coleccion = basededatos.GetCollection<BsonDocument>("Personas");
+
+            var persona = new BsonDocument { { "Nombre","Desde C#" } };
+
+            coleccion.InsertOne(persona);
+
 
         }
     }
