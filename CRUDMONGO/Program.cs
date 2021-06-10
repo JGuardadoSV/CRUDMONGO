5961ce8c83a7d2315a821942a1f241ba95619448
JGuardadoSV <47751227+JGuardadoSV@users.noreply.github.com>
2021-06-10 14:05:45 -0600

    Agregar archivos de proyecto.

---
+++ CRUDMONGO/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace CRUDMONGO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena = "mongodb://localhost:27017/";
+
+            MongoClient cliente = new MongoClient(cadena);
+
+
+        }
+    }
+}
